e6d1fd52cb85a85c3f34dcb36bc4c0956c3c7c4e
ssmendon <31165513+ssmendon@users.noreply.github.com>
2020-11-26 18:45:49 +0000

    Prototyping from PyTorch

--- mendon/nn.py
+++ mendon/nn.py
@@ -1,6 +1,61 @@
-class Network(object):
-    def __init__(self, in_features: int, out_features: int, bias: bool = True) -> None:
+from abc import ABC
+import numpy as np
+
+class Unit(ABC):
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
         pass
 
-class Perceptron(object):
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        forward(x)
+
+class Network(Unit):
+    r'''Represents a 3-layer MLP with configurable layer sizes.'''
+
+    in_features: int
+    out_features: int
+    hidden_size: int
+
+    def __init__(self, in_features: int, out_features: int, hidden_size: int) -> None:
+        self.in_features = in_features
+        self.out_features = out_features
+        self.hidden_size = hidden_size
+
+        model = [
+            Perceptron((in_features, hidden_size)),
+            Sigmoid(),
+            Perceptron((hidden_size, out_features)),
+            Sigmoid()
+        ]
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        y = x
+        for layer in model:
+            y = layer(y)
+        return y
+
+class Perceptron(Unit):
+    r'''A light wrapper around a matrix to represent a perceptron.'''
+    
+    in_features: int
+    out_features: int
+    weight: np.ndarray
+    bias: np.ndarray
+
+    def __init__(self, in_features: int, out_features: int) -> None:
+        self.in_features = in_features
+        self.out_features = out_features
+
+        self.weight = np.ndarray((in_features, out_features))
+        self.bias = np.ndarray((out_features))
+
+        self.initialize_parameters()
+
+    def initialize_parameters(self) -> None:
+        pass
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        return np.matmul(x, self.weight.T) + self.bias
+
+class Sigmoid(Unit):
     pass
