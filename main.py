ad5128d5b1e963e89c6e4f6990bc66375487bce4
ssmendon <31165513+ssmendon@users.noreply.github.com>
2020-12-08 14:20:57 +0000

    Experimentation

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from mendon.nn import Perceptron, Network
 import numpy as np
+import numpy.matlib
 
 def main():
-    n = Network(2, 1, 2)
-    
+    backprop_test()
+    return
+
     x = np.array([
         [-1, -1],
         [-1, 1],
         [1, -1],
         [1, 1]
+    ])
+
+    t = np.array([
+        -1, 1, 1, -1
     ])
 
     # add bias
@@ -48,8 +54,59 @@ def main():
     outputs = np.sign(net_k)
 
     print(outputs)
+    print()
 
     return
+
+def backprop_test():
+    np.random.seed(0)
+
+    x = np.array([
+        [-1, -1],
+        [-1, 1],
+        [1, -1],
+        [1, 1]
+    ])
+
+    t = np.array([
+        -1, 1, 1, -1
+    ])
+
+    activation = lambda x: 1 / (1 - np.exp(-x))
+    activation_deriv = lambda x: activation(x) * (1 - activation(x))
+
+    error_func = lambda z, t: np.sum(np.square(t - z), axis=0)/2
+    error_deriv = lambda y, z, t: -np.sum(t - z) * activation_deriv(y)
+
+    
+    errors = np.ones((1, len(x)))
+    
+    while np.linalg.norm(errors) >= 0.00001:
+        # add bias
+        bias = np.ones(
+            (x.shape[0], 1)
+        )
+
+        x = np.concatenate((bias, x), axis=1)
+        x = x.transpose()
+
+        weights = np.matlib.rand(3, 3)
+
+        net_j = np.matmul(weights[:,0:2].T, x)
+
+        y = activation(net_j)
+
+        y = np.concatenate((bias, y.T), axis=1).T  # reattach bias
+
+
+        net_k = np.matmul(weights[:,2:].T, y)
+        outputs = activation(net_k)
+
+        # backprop starts here
+        errors = error_func(outputs, t)
+
+        break
+
 
 
 if __name__ == '__main__':
--- mendon/nn.py
+++ mendon/nn.py
@@ -1,5 +1,14 @@
 from abc import ABC
+from itertools import tee
+
 import numpy as np
+
+# from itertools docs
+def _pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
 
 class Unit(ABC):
 
@@ -7,7 +16,7 @@ class Unit(ABC):
         pass
 
     def __call__(self, x: np.ndarray) -> np.ndarray:
-        forward(x)
+        self.forward(x)
 
 class Network(Unit):
     r'''Represents a 3-layer MLP with configurable layer sizes.'''
@@ -15,24 +24,45 @@ class Network(Unit):
     in_features: int
     out_features: int
     hidden_size: int
+    model: [np.ndarray]
 
     def __init__(self, in_features: int, out_features: int, hidden_size: int) -> None:
         self.in_features = in_features
         self.out_features = out_features
         self.hidden_size = hidden_size
 
-        model = [
-            Perceptron((in_features, hidden_size)),
-            Sigmoid(),
-            Perceptron((hidden_size, out_features)),
-            Sigmoid()
-        ]
+        self.error = lambda z, t: 1/2 * np.square(np.linalg.norm(z - t))
+        self.error_deriv = lambda z, t: -(z - t)
+
+        self.model = []
 
     def forward(self, x: np.ndarray) -> np.ndarray:
         y = x
-        for layer in model:
+        for layer in self.model:
             y = layer(y)
         return y
+
+    def backward_online(self, x: np.ndarray, t: np.ndarray, eta: float) -> np.ndarray:
+        r'''A method for doing online backpropagation.'''
+
+        # need the inputs and outputs from each layer
+        y = x
+        net = []
+        out = []
+
+        # each layer in this MLP is a linear layer followed by nonlinear activation
+        for linear, nonlinear in self.model:
+            y = linear(y)
+            net.append(y)
+
+            y = nonlinear(y)
+            out.append(y)
+
+        # compute output layer error
+        delta = -eta * self.error_deriv(out[-1], t) * self.model[-1].derivative(net[-1])
+
+        print(delta)
+            
 
 class Perceptron(Unit):
     r'''A light wrapper around a matrix to represent a perceptron.'''
@@ -42,20 +72,61 @@ class Perceptron(Unit):
     weight: np.ndarray
     bias: np.ndarray
 
-    def __init__(self, in_features: int, out_features: int) -> None:
+    def __init__(self, in_features: int, out_features: int, weight: np.ndarray = None, bias: np.ndarray = None) -> None:
+        r'''Initializes the perceptron weights (accounting for a bias term).'''
         self.in_features = in_features
         self.out_features = out_features
 
-        self.weight = np.ndarray((in_features, out_features))
-        self.bias = np.ndarray((out_features))
+        self.initialize_parameters(weight, bias)
 
-        self.initialize_parameters()
+    def initialize_parameters(self, weight: np.ndarray, bias: np.ndarray) -> None:
+        r'''Initializes the weights and bias parameters.'''
 
-    def initialize_parameters(self) -> None:
-        pass
+        if weight and weight.shape == (self.in_features, self.out_features):
+            self.weight = weight
+        else:
+            raise ValueError('Initializing weights failed with invalid shape')
+
+        if bias and bias.shape == (self.out_features, 1):
+            self.bias = bias
+        else:
+            raise ValueError('Initializing bias failed with invalid shape')
+
+        # random initialization, see 6.8.8 from text
+        b = 1/np.sqrt(self.out_features)
+        a = -1/np.sqrt(self.out_features)
+        self.weight = (b - a) * np.random.random_sample((self.in_features, self.out_features)) + a
+        self.bias = (b - a) * np.random.random_sample((self.out_features, 1)) + a
 
     def forward(self, x: np.ndarray) -> np.ndarray:
         return np.matmul(x, self.weight.T) + self.bias
 
 class Sigmoid(Unit):
-    pass
+    r'''A class representing the Sigmoid activation function.'''
+
+    def __init__(self) -> None:
+        self.activation = lambda x: 1 / (1 + np.exp(-x))
+        self.derivative = lambda x: self.activation(x) * (1 - self.activation(x))
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        return self.activation(x)
+
+class ReLU(Unit):
+    r'''A class representing the ReLU activation function.'''
+
+    def __init__(self) -> None:
+        self.activation = lambda x: max(0, x)
+        self.derivative = lambda x: 1 if x else 0
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        return self.activation(x)
+
+class Sgn(Unit):
+    r'''A class representing the sign activation function.'''
+
+    def __init__(self) -> None:
+        self.activation = np.sign
+        self.derivative = None
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        return self.activation(x)
