f0411b2f438582b43088c093a5f6d7157dc7417c
ssmendon <31165513+ssmendon@users.noreply.github.com>
2020-12-09 21:52:09 +0000

    Added graphing code to obtain learning curve, accuracy

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ import numpy as np
 import numpy.matlib
 import matplotlib.pyplot as pyplot
 
+pyplot.rcParams['figure.figsize'] = [7.4, 5.8]
+pyplot.rcParams['figure.dpi'] = 150
 
 def main():
 
     # load the data
-    class1, labels1 = load_data('./data/Train1.txt', 0)
-    class2, labels2 = load_data('./data/Train2.txt', 1)
+    class1 = np.loadtxt('./data/Train1.txt')
+    class2 = np.loadtxt('./data/Train2.txt')
+
+    test1 = np.loadtxt('./data/Test1.txt')
+    test2 = np.loadtxt('./data/Test2.txt')
 
     # normalization
     normalized_data = normalize(
@@ -20,42 +25,134 @@ def main():
         np.vstack([class1, class2]).transpose(),
     )
 
-    # separate train and validation
+    # normalize testing set
+    testX = normalize(
+        np.vstack([class1[:1500,:], class2[:1500,:]]).transpose(),
+        np.vstack([test1, test2]).transpose(),
+    )
+    testY = np.vstack([np.zeros((1000, 1)), np.ones((1000, 1))])
+
+    # separate train from validation
     trainX = np.hstack([
         normalized_data[:,:1500], normalized_data[:,2000:3500]
     ])
     trainY = np.vstack([np.zeros((1500,1)), np.ones((1500,1))])
 
-    testX = np.hstack([
+    validationX = np.hstack([
         normalized_data[:,1500:2000], normalized_data[:,3500:]
     ])
-    testY = np.vstack([np.zeros((500,1)), np.ones((500, 1))])
+    validationY = np.vstack([np.zeros((500,1)), np.ones((500, 1))])
 
     # train
+    eta = 0.1 #eta = 0.002
     hidden_sizes = [2, 4, 6, 8, 10]
-    models = defaultdict(lambda: [])
+    models = defaultdict(list)
     for size in hidden_sizes:
-        for i in range(10):
+        for i in range(10):  # run each model 10 times
             print('Training with size {} on iter {}'.format(size, i))
 
             n = Network(2, size, 1)
-            train(trainX, trainY, testX, testY, 0.002, n)
+            losses = train(trainX, trainY, validationX, validationY, testX, testY, eta, n)
 
             models[size].append(n)
 
-            # save a contour plot of the decision boundary
+            # save a contour plot of the decision boundary for the training data
+            # and the learning curves
             plot_decision_boundary(
-                './img/size-{}-iter-{}.jpg'.format(size, i), 
+                './img/decision-boundary/size-{}-iter-{}.jpg'.format(size, i), 
                 normalized_data, 
                 np.vstack([np.zeros((2000,1)), np.ones((2000, 1))]).astype(int), 
                 n
             )
+            plot_learning_curves(
+                './img/learning-curves/size-{}-iter{}.jpg'.format(size, i),
+                losses
+            )
+            
         
         print('\n')
     
+    print('Evaluating model performance...')
     # evaluate performance of models
+    # use testing set and take the average of all 10 models
+    # also: take the best, for comparison
+    avg_accuracies = []
+    best_accuracies = []
+    for size in hidden_sizes:
+        nets = models[size]
 
-def plot_decision_boundary(file: str, data: np.ndarray, labels: np.ndarray, n: Network):
+        accuracies = np.array([np.mean(n(testX).T.round() == testY) for n in nets])
+        avg_accuracies.append(np.mean(accuracies))
+        best_accuracies.append(np.amax(accuracies))
+
+    plot_accuracy('./img/accuracies/chart.jpg', hidden_sizes, avg_accuracies, best_accuracies)
+
+    print('Size'.rjust(25), end='')
+    print('Average'.rjust(20), end='')
+    print('Best'.rjust(20))
+    for i in range(len(hidden_sizes)):
+        print(str(hidden_sizes[i]).rjust(25), end='')
+        print('{:.4%}'.format(avg_accuracies[i]).rjust(20), end='')
+        print('{:.4%}'.format(best_accuracies[i]).rjust(20))
+
+
+def plot_accuracy(file: str, hidden_sizes: list[int], avg_accuracies: list[float], best_accuracies: list[float]) -> None:
+    r'''Plots the accuracy vs. hidden layer size.
+
+    Args:
+        file: path to where the file is saved (directories should already exist)
+        hidden_sizes: a list of what hidden sizes were used
+        avg_accuracies: a list of the average accuracy for that hidden layer size
+        best_accuracies: a list of the best accuracy for that hidden layer size
+    '''
+    
+    fig, ax = pyplot.subplots()
+
+    ax.plot(hidden_sizes, avg_accuracies, 'r--', label='Average Loss')
+    ax.plot(hidden_sizes, best_accuracies, 'b--', label='Best Loss')
+
+    ax.set(xlabel='hidden size', ylabel='accuracy (%)',
+           title='Accuracy')
+
+    ax.legend()
+
+    fig.savefig(file)
+    pyplot.close(fig)
+
+
+def plot_learning_curves(file: str, losses: dict[str, list[int, ...]]) -> None:
+    r'''Plots the learning curves for a given set of train, validation, and testing losses.
+
+    The code is adapted from the matplotlib sample:
+    https://matplotlib.org/3.3.3/gallery/lines_bars_and_markers/simple_plot.html
+
+    Args:
+        file: path where the file is saved (directories should already exist)
+        losses: a dictionary with keys 'train', 'validation', and 'test' storing a list of loss/n_samples
+            (starting at epoch = 1 until epoch = len(list))
+    '''
+
+    num_epochs = len(losses['train'])
+    if not num_epochs:
+        print('No epochs to plot')
+    epochs_axis = np.arange(0, num_epochs, 1, dtype=int)
+
+    fig, ax = pyplot.subplots()
+
+    ax.plot(epochs_axis, losses['train'], 'ro--', label='Train loss')
+    ax.plot(epochs_axis, losses['validation'], 'bo--', label='Validation loss')
+    ax.plot(epochs_axis, losses['test'], 'go--', label='Testing loss')
+
+    ax.set(xlabel='epochs', ylabel='J/n',
+            title='Learning Curve')
+
+    ax.legend()
+
+    fig.savefig(file)
+    pyplot.close(fig)
+    
+
+def plot_decision_boundary(file: str, data: np.ndarray, labels: np.ndarray, n: Network) -> None:
     r'''Plots the decision region given a trained model and labelled data.
 
     This is adapted from my Project 1 code, which in turn was adapted from:
@@ -96,7 +193,7 @@ def plot_decision_boundary(file: str, data: np.ndarray, labels: np.ndarray, n: N
     # contourf chooses a pair of indices i,j from xx and yy and checks index zz(i, j) = xx(i, j), yy(i, j)
     zz = predictions.reshape(xx.shape)
 
-    fig = pyplot.figure(figsize=(15,10))
+    fig = pyplot.figure()
     pyplot.contourf(xx, yy, zz, cmap='Paired')  # decision boundary
 
     # draw the points in data colored by their class
@@ -121,6 +218,12 @@ def plot_decision_boundary(file: str, data: np.ndarray, labels: np.ndarray, n: N
     pyplot.close()
 
 def normalize(train: np.ndarray, combined: np.ndarray) -> np.ndarray:
+    r'''Use the training set to normalize the combined data.
+
+    Args:
+        train: column vector (feat x samples) of data for mean, std computation
+        combined: the data to normalize with std. normalization
+    '''
 
     # mean and std of each feature
     # each row is one feature
@@ -133,20 +236,40 @@ def normalize(train: np.ndarray, combined: np.ndarray) -> np.ndarray:
 
     return (combined - mean)/std
 
-def load_data(file: str, label: int) -> (np.ndarray, np.ndarray):
-    data = np.loadtxt(file)
-    num_samples = data.shape[0]
-    labels = np.ones((num_samples,1)) if label else np.zeros((num_samples, 1))
+def train(
+    trainX: np.ndarray,
+    trainY: np.ndarray,
+    validationX: np.ndarray,
+    validationY: np.ndarray,
+    testX: np.ndarray,
+    testY: np.ndarray,
+    eta: float, 
+    n: Network,
+    ) -> dict[str, list[float, ...]]:
+    r'''Train the network with the given learning parameter.
 
-    return data, labels
+    It uses the arguments to construct and save plots for the report.
 
-def train(trainX, trainY, testX, testY, eta, n):
+    Args:
+        trainX, trainY: the set to train on + labels
+        validationX, validationY: the set to determine when to stop training
+        testX, testY: the set to evaluate model accuracy with
+        eta: a learning parameter
+        n: the neural network
+    '''
 
     n_samples = trainX.shape[1]
     epochs = 0
 
-    prev_err = n.error(n(testX).T, testY)/n_samples
-    epsilon = 0.000002
+    # add epoch 0 to this list
+    losses = defaultdict(list)
+
+    prev_err = n.error(n(validationX).T, validationY)/n_samples
+    epsilon = 0.0002
+
+    losses['train'].append(n.error(n(trainX).T, trainY)/n_samples)
+    losses['validation'].append(prev_err)
+    losses['test'].append(n.error(n(testX).T, testY)/n_samples)
 
     done = False
     while not done:
@@ -154,48 +277,64 @@ def train(trainX, trainY, testX, testY, eta, n):
         order = list(range(n_samples))
         np.random.shuffle(order)
 
-        # single sample training
-        # pick a random number of samples to individually run and update
         
         for j in order:
             x = trainX[:, j]
             x = x[..., np.newaxis]
             t = trainY[j]
-            n.backward_online(x, t, eta)
+            n.backward_online(x, t, eta)  # single sample training
 
         epochs += 1
 
         # stopping condition
-        # error not decreasing much anymore
-        # could use np.linalg.norm(n.error_deriv) < epsilon, using tangent line approximation instead
-        fwd = n(testX).transpose()
-        err = n.error(fwd, testY)/n_samples
+        # error not decreasing much anymore from one epoch to the next
+        # may continue if loss increases slightly since it's an absolute difference
+        # but in validation the loss will usually continue to decrease after this
+        fwd = n(validationX).transpose()
+        err = n.error(fwd, validationY)/n_samples
         if np.abs(err - prev_err) <= epsilon:
             done = True  # just to print accuracy one final time
         else:
             prev_err = err
 
-        # accuracy
-        if not epochs % 10 or done:
-            predict_train = n(trainX).transpose()
-            predict_validation = fwd
+        
+        # save for learning curve
+        predict_train = n(trainX).transpose()
+        predict_validation = fwd
+        predict_test = n(testX).transpose()
 
+
+        # print accuracy every 10 epochs
+        if not epochs % 10 or done:
             train_accuracy = predict_train.round() == trainY
             train_accuracy = np.mean(train_accuracy)
-            validation_accuracy = predict_validation.round() == testY
+            validation_accuracy = predict_validation.round() == validationY
             validation_accuracy = np.mean(validation_accuracy)
+            test_accuracy = predict_test.round() == testY
+            test_accuracy = np.mean(test_accuracy)
 
             prompt = 'Training set accuracy after {} epochs:'.format(epochs).ljust(45)
             print(prompt + '{:.4%}'.format(train_accuracy))
 
             prompt = 'Validation set accuracy after {} epochs:'.format(epochs).ljust(45)
             print(prompt + '{:.4%}'.format(validation_accuracy))
+
+            prompt = 'Testing set accuracy after {} epochs:'.format(epochs).ljust(45)
+            print(prompt + '{:.4%}'.format(test_accuracy))
+
             print()
+
+        # save the loss/n_samples for later plot creation
+        losses['train'].append(n.error(predict_train, trainY)/n_samples)
+        losses['validation'].append(err)
+        losses['test'].append(n.error(predict_test, testY)/n_samples)
 
         # kill condition
         # learning rate is too high and we're oscillating
-        if epochs >= 300:
+        if epochs >= 200:
             done = True
+
+    return losses
 
 
 if __name__ == '__main__':
